506eb3fc5cc1262c0e33f06857de62857db7d3aa
ChloeMonv <cmonvoisi003@ensc.fr>
2023-10-19 10:36:12 +0200

    dans la bdd

--- EFTodo/Program.cs
+++ EFTodo/Program.cs
@@ -1,3 +1,15 @@
-﻿Todo todo1 = new Todo(1, "Manger", false);
+﻿
+TodoContext context = new TodoContext();
+
+Todo todo1 = new Todo(1, "Manger", false);
 Todo todo2 = new Todo(2, "Pilotage", false);
 Todo todo3 = new Todo(3, "TBM", true);
+
+
+
+
+// context.Add(todo1);
+// context.Add(todo2);
+// context.Add(todo3);
+
+// context.SaveChanges();

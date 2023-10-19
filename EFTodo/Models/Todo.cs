b296e59637705004d3d87dea5317517e157ba06c
ChloeMonv <cmonvoisi003@ensc.fr>
2023-10-19 10:14:13 +0200

    enregistre tes fichiers chloé

---
+++ EFTodo/Models/Todo.cs
@@ -0,0 +1,18 @@
+public class Todo
+{
+
+    public int Id;
+
+    public string Task;
+
+    public bool Completed;
+
+
+    public Todo(int id, string task, bool completed)
+    {
+        Id = id;
+        Task = task;
+        Completed = completed;
+
+    }
+}

130b8de8641043c35beafeaf9be888f75bf18d1a
ChloeMonv <cmonvoisi003@ensc.fr>
2023-10-19 12:01:59 +0200

    c'est cassé

--- EFTodo/Program.cs
+++ EFTodo/Program.cs
@@ -1,14 +1,9 @@
 ﻿
 TodoContext context = new TodoContext();
 
-
-
-
 Todo todo1 = new Todo(1, "Manger", false);
 Todo todo2 = new Todo(2, "Pilotage", false);
 Todo todo3 = new Todo(3, "TBM", true);
-
-List<Todo> liste = new List<Todo>();
 
 context.Add(todo1);
 context.Add(todo2);
@@ -17,7 +12,7 @@ context.Add(todo3);
 context.SaveChanges();
 
 
-todo3.Date = DateTime.Now;
+// todo3.Date = DateTime.Now;
 
 Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~Ensemble des Todo ~~~~~~~~~~~~~~~~~~~~~~~~");
 
@@ -29,10 +24,10 @@ foreach (Todo t in context.Todo)
 
 Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~Todo a faire ~~~~~~~~~~~~~~~~~~~~~~~~");
 
-foreach (Todo t in context.Todo.Where(e => e.Completed == false))
+foreach (Todo t in context.Todo)
 {
 
-    Console.WriteLine($"ID : {t.Id} , Tache : {t.Task} , Completé : {t.Completed}");
+    if (t.Completed == false) Console.WriteLine($"ID : {t.Id} , Tache : {t.Task} , Deadline : {t.Date}");
 
 }
 
